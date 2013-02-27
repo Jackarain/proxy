d53d04b7a0a3dd5389e50a1cec18bc82e63b7f0e
Jack <jack.wgm@gmail.com>
2013-02-27 18:21:39 +0800

    implement socks5 server
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "socks_server.hpp"
 
 int main(int argc, char **argv)
 {
+	boost::asio::io_service io_service;
+
+	socks::socks_server s(io_service, 6543);
+
+	io_service.run();
 	return 0;
 }
