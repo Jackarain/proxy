d33feb3c112965c7f7ded8461dfd8cc66d6314bd
jk <jk@example.com>
2017-09-18 14:43:37 +0800

    update js for auth.

--- do_auth.js
+++ do_auth.js
@@ -1,8 +1,31 @@
 
 // javascript auth.
-var do_auth = function(remote_info, name, passwd)
+
+
+// for socks4 server auth.
+var do_auth4 = function(client, name)
 {
-     if (name == "aaa" && passwd == "ccc")
-        return true;
-     return false;
+	var auth = false;
+
+    if (name == "aaa")
+		auth = true;
+
+	print("do_auth4, client: ", client, ", name='", name, "'",
+        (auth ? ", auth passed." : ", auth fail!"));
+
+	return auth;
+};
+
+// for socks5 server auth.
+var do_auth = function(client, name, passwd)
+{
+    var auth = false;
+
+    if (name == "aaa" && passwd == "ccc")
+        auth = true;
+
+    print("do_auth, client: ", client, ", name='", name, "'",
+        (auth ? ", auth passed." : ", auth fail!"));
+
+    return auth;
 };
