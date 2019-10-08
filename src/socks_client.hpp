4ec9d89a0e9cf65080b8a2c322be817e7fbf24f0
Jack <jack.wgm@gmail.com>
2019-10-08 20:35:42 +0800

    fix block ipv6 crash

--- src/socks_client.hpp
+++ src/socks_client.hpp
@@ -409,7 +409,7 @@ namespace socks {
 			if (content.scheme == "socks5")
 			{
 				auto self = shared_from_this();
-				boost::asio::spawn(m_socket.get_io_service(), [this, self, handler]
+				boost::asio::spawn(m_socket.get_executor(), [this, self, handler]
 				(boost::asio::yield_context yield)
 				{
 					do_socks5<Handler>(handler, yield);
@@ -418,7 +418,7 @@ namespace socks {
 			else if (content.scheme == "socks4")
 			{
 				auto self = shared_from_this();
-				boost::asio::spawn(m_socket.get_io_service(), [this, self, handler]
+				boost::asio::spawn(m_socket.get_executor(), [this, self, handler]
 				(boost::asio::yield_context yield)
 				{
 					do_socks4<Handler>(handler, yield);
