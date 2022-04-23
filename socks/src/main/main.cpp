ef6f851ed348e0913f79ae10f57be20af09d561b
jack <jack.wgm@gmail.com>
2022-04-23 14:04:16 +0800

    Add socks server example

--- socks/src/main/main.cpp
+++ socks/src/main/main.cpp
@@ -1,7 +1,52 @@
 ﻿#include "socks/logging.hpp"
+#include "socks/socks_server.hpp"
+#include "socks/socks_client.hpp"
+
+#include "socks/use_awaitable.hpp"
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+
+#include <memory>
+
+namespace net = boost::asio;
+using net::ip::tcp;
+using namespace socks;
+
+using server_ptr = std::shared_ptr<socks_server>;
+
+boost::asio::awaitable<void> start_socks_server(server_ptr& server)
+{
+	tcp::endpoint socks_listen(
+		net::ip::address::from_string("0.0.0.0"),
+		1080);
+
+	socks_server_option opt;
+	opt.usrdid_ = "jack";
+	opt.passwd_ = "1111";
+
+	auto executor = co_await net::this_coro::executor;
+	server =
+		std::make_shared<socks_server>(
+			executor, socks_listen, opt);
+	server->open();
+
+	co_return;
+}
+
+boost::asio::awaitable<void> start_socks_client()
+{
+	co_return;
+}
 
 int main()
 {
-	LOG_DBG << "start main...";
+	net::io_context ioc(1);
+	server_ptr server;
+
+	co_spawn(ioc, start_socks_server(server), net::detached);
+
+	ioc.run();
 	return 0;
 }
