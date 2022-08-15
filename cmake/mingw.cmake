07b0768a573f619086286061ee23e905adffe1a7
Jackarain <jack.wgm@gmail.com>
2022-08-16 07:17:02 +0800

    Support mingw cross compile

---
+++ cmake/mingw.cmake
@@ -0,0 +1,20 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+# x86_64-w64-mingw32-g++
+
+set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+
+# where is the target environment located
+set(CMAKE_FIND_ROOT_PATH /usr/bin)
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
