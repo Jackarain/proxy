af1fab45b85dcfbe0cea2f3fea0eb93a09149884
jack <jack.wgm@gmail.com>
2022-08-16 15:20:11 +0800

    Fix asio.cpp

--- third_party/boost/asio.cpp
+++ third_party/boost/asio.cpp
@@ -1,5 +1,5 @@
 ﻿#include <boost/asio/impl/src.hpp>
 
-#if __has_include(<openssl / ssl.h>) && defined (HAVE_OPENSSL)
+#if __has_include(<openssl/ssl.h>) && defined (HAVE_OPENSSL)
 #	include <boost/asio/ssl/impl/src.hpp>
 #endif
