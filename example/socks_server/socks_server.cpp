e5c3cff1409955ac53d63135dd597df375e321d1
jack <jack.wgm@gmail.com>
2022-08-31 20:46:09 +0800

    Add platform_init() for socks server

--- example/socks_server/socks_server.cpp
+++ example/socks_server/socks_server.cpp
@@ -24,6 +24,27 @@ namespace po = boost::program_options;
 
 #include <memory>
 
+#ifdef __linux__
+#  include <sys/resource.h>
+
+# ifndef HAVE_UNAME
+#  define HAVE_UNAME
+# endif
+
+#elif _WIN32
+#  ifndef WIN32_LEAN_AND_MEAN
+#    define WIN32_LEAN_AND_MEAN
+#  endif
+#  include <fcntl.h>
+#  include <io.h>
+#  include <windows.h>
+
+#endif // _WIN32
+
+#ifdef HAVE_UNAME
+#  include <sys/utsname.h>
+#endif
+
 namespace net = boost::asio;
 using net::ip::tcp;
 using namespace socks;
@@ -147,6 +168,66 @@ inline bool parse_endpoint_string(std::string_view str,
 
 //////////////////////////////////////////////////////////////////////////
 
+inline int platform_init()
+{
+#if defined(WIN32) || defined(_WIN32)
+	/* Disable the "application crashed" popup. */
+	SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX |
+		SEM_NOOPENFILEERRORBOX);
+
+#if defined(DEBUG) ||defined(_DEBUG)
+	//	_CrtDumpMemoryLeaks();
+	// 	int flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+	// 	flags |= _CRTDBG_LEAK_CHECK_DF;
+	// 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	// 	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	// 	_CrtSetDbgFlag(flags);
+#endif
+
+#if !defined(__MINGW32__)
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_DEBUG);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+#endif
+
+	_setmode(0, _O_BINARY);
+	_setmode(1, _O_BINARY);
+	_setmode(2, _O_BINARY);
+
+	/* Disable stdio output buffering. */
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	/* Enable minidump when application crashed. */
+#elif defined(__linux__)
+	rlimit of = { 50000, 100000 };
+	setrlimit(RLIMIT_NOFILE, &of);
+
+	struct rlimit core_limit;
+	core_limit.rlim_cur = RLIM_INFINITY;
+	core_limit.rlim_max = RLIM_INFINITY;
+	setrlimit(RLIMIT_CORE, &core_limit);
+
+	/* Set the stack size programmatically with setrlimit */
+	rlimit rl;
+	int result = getrlimit(RLIMIT_STACK, &rl);
+	if (result == 0)
+	{
+		const rlim_t stack_size = 100 * 1024 * 1024;
+		if (rl.rlim_cur < stack_size)
+		{
+			rl.rlim_cur = stack_size;
+			setrlimit(RLIMIT_STACK, &rl);
+		}
+	}
+#endif
+
+	std::ios::sync_with_stdio(false);
+
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
 net::awaitable<void> start_socks_server(server_ptr& server)
 {
 	std::string host, port;
@@ -184,8 +265,11 @@ net::awaitable<void> start_socks_server(server_ptr& server)
 
 int main(int argc, char** argv)
 {
+	platform_init();
+
 	po::options_description desc("Options");
 	desc.add_options()
+		("help,h", "Help message.")
 		("socks_server", po::value<std::string>(&socks_listen)->default_value("[::0]:1080")->value_name("ip:port"), "For socks4/5 server listen.")
 		("socks_userid", po::value<std::string>(&socks_userid)->default_value("jack")->value_name("userid"), "Socks4/5 auth user id.")
 		("socks_passwd", po::value<std::string>(&socks_passwd)->default_value("1111")->value_name("passwd"), "Socks4/5 auth password.")
