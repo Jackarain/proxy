cf28163fb7efdcd3c19fa48f42bd3356ba3e61f9
jack <jack.wgm@gmail.com>
2023-01-05 16:45:33 +0800

    Fi beast websocket warning

--- third_party/boost/boost/beast/websocket/detail/impl_base.hpp
+++ third_party/boost/boost/beast/websocket/detail/impl_base.hpp
@@ -1,4 +1,4 @@
-//
+﻿//
 // Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -452,7 +452,7 @@ struct impl_base<false>
         return false;
     }
 
-    bool should_compress(std::size_t n_bytes) const
+    bool should_compress(std::size_t) const
     {
         return false;
     }
