ef6f32e7ac4fdda40a85464ef0d07b8f7b69ac73
jack <jack.wgm@gmail.com>
2023-01-30 20:35:29 +0800

    Improve server

---
+++ server/proxy_server/main.hpp
@@ -0,0 +1,121 @@
+﻿//
+// main.hpp
+// ~~~~~~~~
+//
+// Copyright (c) 2023 Jack (jack dot wgm at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <iterator>
+
+//////////////////////////////////////////////////////////////////////////
+
+inline bool is_space(const char c)
+{
+	if (c == ' ' ||
+		c == '\f' ||
+		c == '\n' ||
+		c == '\r' ||
+		c == '\t' ||
+		c == '\v')
+		return true;
+	return false;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+inline std::string_view string_trim(std::string_view sv)
+{
+	const char* b = sv.data();
+	const char* e = b + sv.size();
+
+	for (; b != e; b++)
+	{
+		if (!is_space(*b))
+			break;
+	}
+
+	for (; e != b; )
+	{
+		if (!is_space(*(--e)))
+		{
+			++e;
+			break;
+		}
+	}
+
+	return std::string_view(b, e - b);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+inline bool parse_endpoint_string(std::string_view str,
+	std::string& host, std::string& port, bool& ipv6only)
+{
+	ipv6only = false;
+
+	auto address_string = string_trim(str);
+	auto it = address_string.begin();
+
+	bool is_ipv6_address = *it == '[';
+	if (is_ipv6_address)
+	{
+		auto host_end = std::find(it, address_string.end(), ']');
+		if (host_end == address_string.end())
+			return false;
+
+		it++;
+		for (auto first = it; first != host_end; first++)
+			host.push_back(*first);
+
+		std::advance(it, host_end - it);
+		it++;
+	}
+	else
+	{
+		auto host_end = std::find(it, address_string.end(), ':');
+		if (host_end == address_string.end())
+			return false;
+
+		for (auto first = it; first != host_end; first++)
+			host.push_back(*first);
+
+		// Skip host.
+		std::advance(it, host_end - it);
+	}
+
+	if (*it != ':')
+		return false;
+
+	it++;
+	for (; it != address_string.end(); it++)
+	{
+		if (*it >= '0' && *it <= '9')
+		{
+			port.push_back(*it);
+			continue;
+		}
+
+		break;
+	}
+
+	if (it != address_string.end())
+	{
+#ifdef __cpp_lib_to_address
+		auto opt = std::string_view(
+			std::to_address(it), address_string.end() - it);
+#else
+		auto opt = std::string(it, address_string.end());
+#endif
+		if (opt == "ipv6only" || opt == "-ipv6only")
+			ipv6only = true;
+	}
+
+	return true;
+}
