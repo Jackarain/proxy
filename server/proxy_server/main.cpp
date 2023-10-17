81d561835e20e7ed27e16cd19f1e01c635721c8e
jackarain <jack.wgm@gmail.com>
2023-10-17 16:44:58 +0800

    Print built info

--- server/proxy_server/main.cpp
+++ server/proxy_server/main.cpp
@@ -183,6 +183,51 @@ start_proxy_server(net::io_context& ioc, server_ptr& server)
 
 //////////////////////////////////////////////////////////////////////////
 
+inline std::string version_info()
+{
+	std::string os_name;
+
+#ifdef _WIN32
+#ifdef _WIN64
+	os_name = "Windows (64bit)";
+#else
+	os_name = "Windows (32bit)";
+#endif // _WIN64
+
+#elif defined(HAVE_UNAME)
+	utsname un;
+	uname(&un);
+	os_name = un.sysname;
+	os_name += " ";
+	os_name += un.release;
+
+	// extract_linux_version from un.release;
+	int ma_ver, mi_ver, patch_ver;
+	sscanf(un.release, "%d.%d.%d", &ma_ver, &mi_ver, &patch_ver);
+
+	if (std::string(un.sysname) == "Linux" && ma_ver < 4)
+	{
+		std::cerr << "WARNING: kernel too old, "
+			<< "please upgrade your system!" << std::endl;
+	}
+
+#elif defined(__APPLE__)
+	os_name = "Drawin";
+#else
+	os_name = "unknow";
+#endif // _WIN32
+
+	std::ostringstream oss;
+	oss << "Built on " << __DATE__
+		<< " " << __TIME__
+		<< " runs on " << os_name
+		<< ", " << BOOST_COMPILER
+		<< ", boost " << BOOST_LIB_VERSION;
+	std::cerr << oss.str() << "\n";
+
+	return oss.str();
+}
+
 void print_args(int argc, char** argv,
 	const po::variables_map& vm)
 {
@@ -287,7 +332,7 @@ int main(int argc, char** argv)
 	// 帮助输出.
 	if (vm.count("help") || argc == 1)
 	{
-		std::cout << desc;
+		std::cout << version_info() << desc;
 		return EXIT_SUCCESS;
 	}
 
