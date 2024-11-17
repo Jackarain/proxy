bd61dc3aa9df4c70ff9c375c40dfa4830b874086
Jackarain <jack.wgm@gmail.com>
2024-11-17 17:19:17 +0800

    Print log certificates expire date

--- proxy/include/proxy/proxy_server.hpp
+++ proxy/include/proxy/proxy_server.hpp
@@ -4960,7 +4960,8 @@ R"x*x*x(<html>
 			for (const auto& ctx : m_certificates)
 			{
 				XLOG_DBG << "domain: '" << ctx.domain_
-					<< "', cert: '" << ctx.cert_.filepath_.string()
+					<< "', expire: '" << ctx.expire_date_
+					<< ", cert: '" << ctx.cert_.filepath_.string()
 					<< "', key: '" << ctx.key_.filepath_.string()
 					<< "', dhparam: '" << ctx.dhparam_.filepath_.string()
 					<< "', pwd: '" << ctx.pwd_.filepath_.string() << "'";
