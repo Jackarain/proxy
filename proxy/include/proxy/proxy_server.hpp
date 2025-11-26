ab6941da760c3156276ffe035a079db6d219c562
Jackarain <jack.wgm@gmail.com>
2025-11-26 23:06:29 +0800

    Update SNI callback to match certificate

--- proxy/include/proxy/proxy_server.hpp
+++ proxy/include/proxy/proxy_server.hpp
@@ -5218,42 +5218,51 @@ R"x*x*x(<html>
 		inline int sni_callback(SSL *ssl, [[maybe_unused]] int *ad) noexcept
 		{
 			const char *servername = SSL_get_servername(ssl, TLSEXT_NAMETYPE_host_name);
-			if (servername)
+			if (!servername)
 			{
-				certificate_file* default_ctx = nullptr;
+				// 返回默认证书.
 				auto& certificates = *m_certificates;
+				if (certificates.empty())
+					return SSL_TLSEXT_ERR_OK;
 
-				for (auto& ctx : certificates)
+				SSL_set_SSL_CTX(ssl, certificates[0].ssl_context_->native_handle());
+				return SSL_TLSEXT_ERR_OK;
+			}
+
+			certificate_file* default_ctx = nullptr;
+			auto& certificates = *m_certificates;
+
+			for (auto& ctx : certificates)
+			{
+				if (ctx.ssl_context_.has_value())
 				{
-					if (ctx.ssl_context_.has_value())
+					if (rfc2818_verification_match_pattern(
+						ctx.domain_.c_str(), ctx.domain_.length(), servername))
+					{
+						SSL_set_SSL_CTX(ssl, ctx.ssl_context_->native_handle());
+						return SSL_TLSEXT_ERR_OK;
+					}
+
+					for (auto& alt_name : ctx.alt_names_)
 					{
 						if (rfc2818_verification_match_pattern(
-							ctx.domain_.c_str(), ctx.domain_.length(), servername))
+							alt_name.c_str(), alt_name.length(), servername))
 						{
 							SSL_set_SSL_CTX(ssl, ctx.ssl_context_->native_handle());
 							return SSL_TLSEXT_ERR_OK;
 						}
-
-						for (auto& alt_name : ctx.alt_names_)
-						{
-							if (rfc2818_verification_match_pattern(
-								alt_name.c_str(), alt_name.length(), servername))
-							{
-								SSL_set_SSL_CTX(ssl, ctx.ssl_context_->native_handle());
-								return SSL_TLSEXT_ERR_OK;
-							}
-						}
 					}
-					if (!default_ctx)
-						default_ctx = &ctx;
 				}
-
-				if (default_ctx)
-				{
-					SSL_set_SSL_CTX(ssl, default_ctx->ssl_context_->native_handle());
-					return SSL_TLSEXT_ERR_OK;
-				}
+				if (!default_ctx)
+					default_ctx = &ctx;
 			}
+
+			if (default_ctx)
+			{
+				SSL_set_SSL_CTX(ssl, default_ctx->ssl_context_->native_handle());
+				return SSL_TLSEXT_ERR_OK;
+			}
+
 			return SSL_TLSEXT_ERR_OK;
 		}
 
