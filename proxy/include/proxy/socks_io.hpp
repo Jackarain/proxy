bfe5bbb3aac21c24ec5f36c4579fa220bfeda97c
Jackarain <jack.wgm@gmail.com>
2026-07-13 17:28:16 +0800

    Constrain templates to integral types and use unsigned arithmetic

--- proxy/include/proxy/socks_io.hpp
+++ proxy/include/proxy/socks_io.hpp
@@ -11,26 +11,28 @@
 #ifndef INCLUDE__2023_10_18__SOCKS_IO_HPP
 #define INCLUDE__2023_10_18__SOCKS_IO_HPP
 
-
+#include <type_traits>
+#include <concepts>
 #include <cstddef>
 
 
 namespace io_util {
 
-	template<typename type, typename source>
+	template<std::integral type, typename source>
 	type read(source& p)
 	{
-		type ret = 0;
+		std::make_unsigned_t<type> ret = 0;
 		for (std::size_t i = 0; i < sizeof(type); i++)
 			ret = (ret << 8) | (static_cast<unsigned char>(*p++));
-		return ret;
+		return static_cast<type>(ret);
 	}
 
-	template<typename type, typename target>
+	template<std::integral type, typename target>
 	void write(type v, target& p)
 	{
+		using uv_type = std::make_unsigned_t<type>;
 		for (auto i = (int)sizeof(type) - 1; i >= 0; i--, p++)
-			*p = static_cast<unsigned char>((v >> (i * 8)) & 0xff);
+			*p = static_cast<unsigned char>((uv_type(v) >> (i * 8)) & 0xff);
 	}
 
 }
