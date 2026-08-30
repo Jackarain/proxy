320cd9a4734ece527e58f8cb4f2b65108b7222b0
Jackarain <jack.wgm@gmail.com>
2026-08-30 22:47:32 +0800

    Remove apps/android/aproxy

--- apps/android/README.md
+++ apps/android/README.md
@@ -1,3 +1,1 @@
 xproxy 是使用 libproxy 实现的一个 android 端的 app。
-
-aproxy 是使用 flutter 和部分 kotlin 实现的 https 代理 proxy_server 的 android 客户端 app，服务端必须是 proxy_server，因为要正常在 android 下工作，需要 proxy_server 的 DoH 支持。
--- apps/android/aproxy/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:aproxy/main.dart';
-
-void main() {
-  testWidgets('app builds', (tester) async {
-    await tester.pumpWidget(const AproxyApp());
-    expect(find.text('proxy 配置'), findsOneWidget);
-  });
-}
